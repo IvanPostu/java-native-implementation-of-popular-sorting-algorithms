ec2c88195e6d541c268cf0b6ef9174d633d61d8f
IvanPostu <ipostu20000127@gmail.com>
2021-01-08 09:27:29 +0200

    Added library for logging - log4j2 and written development configuration for it.

--- run.sh
+++ run.sh
@@ -3,6 +3,9 @@
 projectDirectory=`dirname "$0"`
 
 java -Dfile.encoding=UTF-8 -classpath \
-  "$projectDirectory/build/classes/java/main:$projectDirectory/build/resources/main" \
+  "$projectDirectory/build/classes/java/main:$projectDirectory/build/resources/main:$projectDirectory/build/libs/compile_dependencies/*" \
   com.ivan.app.Main
+
+
+
 
--- src/main/java/com/ivan/app/Main.java
+++ src/main/java/com/ivan/app/Main.java
@@ -3,14 +3,26 @@ package com.ivan.app;
 import java.io.IOException;
 import java.net.URISyntaxException;
 
+import com.ivan.app.configuration.Log4jConfiguration;
 import com.ivan.app.sort.SortAlgorithm;
 import com.ivan.app.sort.SortAlgorithmFactory;
 import com.ivan.app.sort.SortAlgorithmFactory.ImplementationType;
 import com.ivan.app.sort.SortAlgorithmFactory.SortType;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class Main {
+  static {
+    Log4jConfiguration.configure();
+  }
+
+  private static final Logger logger = LogManager.getLogger(Main.class);
 
   public static void main(String[] args) throws IOException, URISyntaxException {
+
+    logger.info("example1");
+    logger.error("example2");
+    
     int []arr = {2, 22, 31, 90, 11, 4, 3, 91, 77, 32, 0, -10, -20, 30, 197, 3};
 
     System.out.println("Unsorted array: ");
